5b02bfa85eac44e43f5e465d6bad77cd4a925259
DESKTOP-247518Q\LENOVO <ahmadi.hoseyn@gmail.com>
2021-12-24 13:57:26 +0330

    End of section 6

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -21,6 +21,7 @@ export class AccountService {
           localStorage.setItem('user', JSON.stringify(user));
           this.currentUserSource.next(user);
         }
+        return user;
       })
     );
   }
@@ -32,6 +33,7 @@ export class AccountService {
           localStorage.setItem('user', JSON.stringify(user));
           this.currentUserSource.next(user); 
         }
+        return user;
       })
     );
   }
