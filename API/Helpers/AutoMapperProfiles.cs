c87ce01b483c5fb071286b0eb158b7fa1423327c
DESKTOP-247518Q\LENOVO <ahmadi.hoseyn@gmail.com>
2021-12-31 14:30:55 +0330

    End of section 8

---
+++ API/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using AutoMapper;
+
+namespace API.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<AppUser, MemberDto>()
+                .ForMember(des => des.PhotoUrl, opt => opt.MapFrom(src => 
+                    src.Photos.FirstOrDefault(x => x.IsMain).Url))
+                .ForMember(des => des.Age, opt => opt.MapFrom(src => 
+                    src.DateOfBirth.CalculateAge()));
+            CreateMap<Photo, PhotoDto>();
+        }
+    }
+}
